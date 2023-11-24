113c93b8d3974e72c0d89c1806ed400fa987ffa0
FatchenkoSofiia <fatcenkosona@gmail.com>
2023-11-24 21:24:59 +0200

    required

--- src 2/js/01-gallery.js
+++ src 2/js/01-gallery.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
+function createGallery () {
+    const galleryList = document.querySelector('.gallery');
+
+    const galleryMarkup = galleryItems.map(
+        ({original, preview, description}) =>
+        `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+        </a>
+      </li>
+    `
+    )
+    .join('');
+
+galleryList.innerHTML = galleryMarkup;
+};
+
+function handleImageClick(event) {
+    event.preventDefault();
+
+    if (event.target.nodeName === 'IMG') {
+        const imageUrl = event.target.dataset.source;
+        const instance = basicLightbox.create(`
+        <img src="${imageUrl}" alt="${event.target.alt}">`);
+        instance.show();
+    }
+}
+
+const galleryList = document.querySelector('.gallery');
+galleryList.addEventListener('click', handleImageClick);
+
+createGallery();
 
 console.log(galleryItems);
